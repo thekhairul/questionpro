87914ca2a95d357d1f7be78d1ea52ed03f8d83a1
thekhairul <thekhairul.office@gmail.com>
2025-05-12 19:48:22 +0600

    setup navigation

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,12 +1,34 @@
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from 'react-router';
+
+const links = [
+  { to: '/dashboard/posts', label: 'Posts' },
+  { to: '/dashboard/comments', label: 'Comments' },
+];
 
 const DashboardLayout: React.FC = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-            <Outlet />
-        </div>
-    );
+  return (
+    <div>
+      <nav className="mb-6">
+        <ul className="flex gap-4">
+          {links.map((link) => (
+            <li key={link.to}>
+              <NavLink
+                to={link.to}
+                className={({ isActive }) =>
+                  `p-2 rounded-sm hover:opacity-85 ${
+                    isActive ? 'bg-blue-700 text-white' : 'text-gray-700'
+                  }`
+                }
+              >
+                {link.label}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
 };
 
 export default DashboardLayout;
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,45 @@
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from 'react-router';
+
+const links = [
+  { to: '/', label: 'Home' },
+  { to: '/dashboard', label: 'Dashboard' },
+  { to: '/component', label: 'My Component' },
+];
 
 const RootLayout: React.FC = () => {
   return (
-    <div>
-      <Outlet />
+    <div className="container mx-auto">
+      <header className="py-4">
+        <nav className="py-2 border-b border-gray-300">
+          <ul className="flex gap-4">
+            {links.map((link) => (
+              <li key={link.to}>
+                <NavLink
+                  to={link.to}
+                  className={({ isActive }) =>
+                    `p-2 rounded-sm hover:opacity-85 ${
+                      isActive ? 'bg-blue-700 text-white' : 'text-gray-700'
+                    }`
+                  }
+                >
+                  {link.label}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <section className="flex justify-between gap-4">
+        <main className="flex-grow">
+          <Outlet />
+        </main>
+        <aside>
+          <div className="p-4 bg-gray-100 rounded-md">
+            <h2 className="text-lg font-semibold">Sidebar</h2>
+            <p>This is the sidebar content.</p>
+          </div>
+        </aside>
+      </section>
     </div>
   );
 };
